6025c446767fd299e5cdde52afd485f64cbb5610
Michael Kim <michaelkim3@hotmail.com>
2017-08-25 23:04:58 -0400

    8/25 Changelog
    - Firebase database integration.
    - Enable adding of students to the database.
    - Database results show up for the listview.

---
+++ byg/addStudent.java
@@ -0,0 +1,63 @@
+package michaelkim.byg;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class addStudent extends AppCompatActivity {
+
+    EditText sName, sBirth, sPhone, sAddress, sCity, sState, sZip;
+    Button addStudent;
+
+    DatabaseReference databaseStudents;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_student);
+
+        sName = (EditText) findViewById(R.id.studentName);
+        sBirth = (EditText) findViewById(R.id.studentBirthday);
+        sPhone = (EditText) findViewById(R.id.studentPhone);
+        sAddress = (EditText) findViewById(R.id.studentAddress);
+        sCity = (EditText) findViewById(R.id.studentCity);
+        sState = (EditText) findViewById(R.id.studentState);
+        sZip = (EditText) findViewById(R.id.studentZip);
+        addStudent = (Button) findViewById(R.id.addStudentButton);
+
+        databaseStudents = FirebaseDatabase.getInstance().getReference("Students");
+
+        addStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = sName.getText().toString().trim();
+                String grade = "10B";
+                String birthday = sBirth.getText().toString().trim();
+                String phone = sPhone.getText().toString().trim();
+                String address = sAddress.getText().toString().trim();
+                String city = sCity.getText().toString().trim();
+                String state = sState.getText().toString().trim();
+                String zip = sZip.getText().toString().trim();
+
+                if (!TextUtils.isEmpty(name)){
+                    String id = databaseStudents.push().getKey();
+                    student newStudent = new student(name, grade, birthday, phone, address, city, state, zip);
+                    databaseStudents.child(id).setValue(newStudent);
+
+                    Toast.makeText(getApplicationContext(), "Student Added.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Please enter a name.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
--- byg/homeDirectory.java
+++ byg/homeDirectory.java
@@ -1,7 +1,9 @@
 package michaelkim.byg;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,50 +12,78 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class homeDirectory extends Fragment {
 
     ListView studentDirectory;
-    ArrayList<String> sampleNames = new ArrayList<>();
-    ArrayList<String> sampleGrades = new ArrayList<>();
-    ArrayList<String> sampleBirthdays = new ArrayList<>();
-    ArrayList<String> sampleAddresses = new ArrayList<>();
+    ArrayList<student> studentList;
+    directoryAdapter directoryAdapter;
+    DatabaseReference databaseStudents;
+
+    FloatingActionButton fab;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_directory, container, false);
 
-        sampleNames.add("Student 1");
-        sampleGrades.add("6B");
-        sampleBirthdays.add("01/10/2013");
-        sampleAddresses.add("12331 Address St., Town, State, Zip");
-        sampleNames.add("Student 2");
-        sampleGrades.add("8G");
-        sampleBirthdays.add("01/10/2011");
-        sampleAddresses.add("543143 Address St., Town, State, Zip");
-        sampleNames.add("Student 3");
-        sampleGrades.add("9G");
-        sampleBirthdays.add("01/10/2010");
-        sampleAddresses.add("3421467 Address St., Town, State, Zip");
-        sampleNames.add("Student 4");
-        sampleGrades.add("12B");
-        sampleBirthdays.add("01/10/2007");
-        sampleAddresses.add("18520 Address St., Town, State, Zip");
+        databaseStudents = FirebaseDatabase.getInstance().getReference("Students");
+        studentList = new ArrayList<>();
+
+        fab = (FloatingActionButton) view.findViewById(R.id.addStudent);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addStudent = new Intent(getContext(), addStudent.class);
+                startActivity(addStudent);
+            }
+        });
 
         studentDirectory = (ListView) view.findViewById(R.id.directory);
-        directoryAdapter directoryAdapter = new directoryAdapter();
         studentDirectory.setAdapter(directoryAdapter);
 
         return view;
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        databaseStudents.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                studentList.clear();
+
+                for (DataSnapshot studentSnapshot : dataSnapshot.getChildren()){
+                    student student = studentSnapshot.getValue(student.class);
+
+                    studentList.add(student);
+                }
+
+                directoryAdapter = new directoryAdapter();
+                studentDirectory.setAdapter(directoryAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     private class directoryAdapter extends BaseAdapter {
 
         @Override
         public int getCount() {
-            return sampleNames.size();
+            return studentList.size();
         }
 
         @Override
@@ -70,15 +100,17 @@ public class homeDirectory extends Fragment {
         public View getView(final int i, View view, ViewGroup viewGroup) {
             view = getActivity().getLayoutInflater().inflate(R.layout.listview_directory, null);
 
-            TextView studentName = (TextView) view.findViewById(R.id.studentName);
-            studentName.setText(sampleNames.get(i));
-            TextView studentGrade = (TextView) view.findViewById(R.id.studentGrade);
-            studentGrade.setText(sampleGrades.get(i));
-            TextView studentBirthday = (TextView) view.findViewById(R.id.studentBirthday);
-            studentBirthday.setText(sampleBirthdays.get(i));
-            TextView studentAddress = (TextView) view.findViewById(R.id.studentAddress);
-            studentAddress.setText(sampleAddresses.get(i));
+            TextView name = (TextView) view.findViewById(R.id.studentName);
+            TextView grade = (TextView) view.findViewById(R.id.studentGrade);
+            TextView birthday = (TextView) view.findViewById(R.id.studentBirthday);
+            TextView address = (TextView) view.findViewById(R.id.studentAddress);
 
+            student student = studentList.get(i);
+
+            name.setText(student.getName());
+            grade.setText(student.getGrade());
+            birthday.setText(student.getBirthday());
+            address.setText(student.getAddress() + ", " + student.getCity() + ", " + student.getState() + ", " + student.getZip());
 
             return view;
         }
